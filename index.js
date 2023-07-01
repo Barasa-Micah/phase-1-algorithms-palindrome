37af57d4b80ce8b8aec6092f970307f5d5ba2d65
Barasa-Micah <barasamica@gmail.com>
2023-07-01 17:19:52 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
-function isPalindrome(word) {
-  // Write your algorithm here
-}
-
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
-}
-
-module.exports = isPalindrome;
+function reverse(word){
+  // const wordArray=word.split("") // splitting it into an array in order to do a reverse
+  // const reversedArray=wordArray.reverse() 
+  // const reversedWord=reversedArray.join("") //joining the reversed array into a string
+  return word.split("").reverse().join("") // cleaning up the code to be more shorter
+  }
+  // function reverse(word){
+  // // const wordArray=word.split("") // splitting it into an array in order to do a reverse
+  // // const reversedArray=wordArray.reverse() 
+  // // const reversedWord=reversedArray.join("") //joining the reversed array into a string
+  // return word.split("").reverse().join("") // cleaning up the code to be more shorter
+  // }
+  
+  
+  function isPalindrome(word) {
+    // Write your algorithm here
+    //changing the string to lowercase
+    //const newString=word.replace(/[^a-zA-Z0-9]/g,"")
+    // const reversedWord =reverse(word)
+    const reversedWord= word.split("").reverse().join("")
+  
+    return (word === reversedWord)
+  }
